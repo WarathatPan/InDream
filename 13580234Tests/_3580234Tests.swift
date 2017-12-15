432bcd79129766e5b8f3bf14c254a3171f4184cf
Krittanai Muangnoi <a2219@isus.local>
2017-12-15 23:20:31 +0700

    Initial Commit

---
+++ 13580234Tests/_3580234Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _3580234Tests.swift
+//  13580234Tests
+//
+//  Created by IMAC on 15/12/2560 BE.
+//  Copyright © 2560 IMAC. All rights reserved.
+//
+
+import XCTest
+@testable import _3580234
+
+class _3580234Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
